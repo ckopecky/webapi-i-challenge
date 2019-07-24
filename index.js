b3eb474e0f3e833a55af83a4d3d162cb1a88c57a
ckopecky <kopecky12112@gmail.com>
2019-07-24 10:01:42 -0700

    Construct GET users endpoint

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ server.get('/', (req, res) => {
     res.send({Success: "API Sanity Check..."})
 })
 
+server.get('/api/users', (req, res) => {
+    Users.find()
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            res.status(500).json({Err: err.message});
+        })
+})
+
